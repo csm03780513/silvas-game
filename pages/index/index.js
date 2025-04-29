ae4f38d7a46f9bf18f97156879c92eaf8c6b3796
csm <carlosmurray1@gmail.com>
2025-04-29 22:26:52 +0300

    init project

---
+++ pages/index/index.js
@@ -0,0 +1,73 @@
+Page({
+  data: {
+    server: 'https://olmercycheperur.com/silva',
+    gameId: '',
+    playerId: '',
+    playerSymbol: '',
+    board: [['', '', ''], ['', '', ''], ['', '', '']],
+    currentTurn: ''
+  },
+
+  onLoad() {
+    this.setData({ playerId: 'player-' + Math.random().toString(36).substr(2, 5) });
+  },
+
+  onGameIdInput(e) {
+    this.setData({ gameId: e.detail.value });
+    this.data.gameId = e.detail.value;
+    console.log("gameId here is:"+`${this.data.gameId}`);
+  },
+
+  createGame() {
+    my.request({
+      url: `${this.data.server}/create`,
+      method: 'POST',
+      success: (res) => {
+        const gameId = res.data.gameId;
+        this.setData({ gameId });
+        this.joinGame(); // auto join as creator
+      }
+    });
+  },
+
+  joinGame() {
+    console.log("gameId here is:"+`${this.data.gameId}`);
+    my.request({
+      url: `${this.data.server}/join/${this.data.gameId}`,
+      method: 'POST',
+      data: { uid: this.data.playerId },
+      success: (res) => {
+        this.setData({ playerSymbol: res.data.symbol });
+        this.pollBoard();
+      }
+    });
+  },
+
+  makeMove(e) {
+    const { row, col } = e.currentTarget.dataset;
+    my.request({
+      url: `${this.data.server}/move/${this.data.gameId}`,
+      method: 'POST',
+      data: {
+        uid: this.data.playerId,
+        row,
+        col
+      },
+      success: (res) => {
+        this.setData({ board: res.data.board, currentTurn: res.data.nextTurn });
+      }
+    });
+  },
+
+  pollBoard() {
+    setInterval(() => {
+      my.request({
+        url: `${this.data.server}/status/${this.data.gameId}`,
+        method: 'GET',
+        success: (res) => {
+          this.setData({ board: res.data.board, currentTurn: res.data.currentTurn });
+        }
+      });
+    }, 1000); // poll every 3s
+  }
+});
